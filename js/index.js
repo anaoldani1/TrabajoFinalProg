e5a4986e5a45b13570a82e5e7c964746c805b69b
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2024-06-04 18:51:43 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+///seccion electronica///
 let categorias = document.querySelector(".category"); 
 let productos = "";
 
@@ -8,7 +10,7 @@ fetch('https://fakestoreapi.com/products/category/electronics')
     })
     .then(function(data){
         console.log(data);
-        for (let i = 0; i<6; i++){
+        for (let i = 0; i<data.length; i++){
             let products = data[i];        
             productos = `
             <h3 class="elemento2">${products.title}</h3>
@@ -25,4 +27,37 @@ fetch('https://fakestoreapi.com/products/category/electronics')
             categorias.innerHTML += productos;
         }
     })
+
+
+    ///seccion hombre///
+    let men= document.querySelector(".catM")
+    let prod=""
+
+    fetch('https://fakestoreapi.com/products/category/men\'s clothing')
+        .then (function(response){
+            return response.json()
+        })
+
+        .then (function(data){
+            console.log(data);
+            for (let i=0; i<data.length; i++){
+
+                let stock= data [i];
+                prod=`
+                <h3 class="elemento2">${stock.title}</h3>
+                <img class="fotos" src="${stock.image}">
+                <p class="elemento3">${stock.description}</p>
+                <p class="elemento4">$${stock.price}</p>
+
+                </form>
+                <form action="./producto.html" method="GET">
+                     <button type="submit">AGREGAR AL CARRITO</button>
+                    <button type="submit">VER MÁS</button>
+                </form>
+                `
+
+                men.innerHTML += prod;
+            }
+        })
+
   
