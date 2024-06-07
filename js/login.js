0f3c9c4107835d3fdc37558e8faa7892895dd964
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2024-06-07 14:10:55 -0300

    login validaciones y local

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let campoEmail= document.querySelector('#email')
+let campoPassword= document.querySelector('#password')
+let form= document.querySelector("#loginForm")
+form.addEventListener('submit',function (evento){
+    evento.preventDefault()
+
+    if (campoEmail.value==""){
+        alert("Porfavor complete el campo de email")
+    }
+    else if (campoPassword.value==""){
+        alert("Porfavor complete el campo de contraseña")
+    }
+    else if (campoPassword.value.length<6){
+        alert("La contraseña tiene que tener al menos 6 caracteres")
+    }else{
+        alert ("Felicidades has podidio completar el formulario")
+    }
+})
+
+sessionStorage.setItem('email', campoEmail.value)
--- js/register.js
+++ js/register.js
@@ -3,9 +3,4 @@ let password = document.getElementById("password").value;
 let repeatPassword = document.getElementById("repeatPassword").value;
 
 
-document.getElementById("registerForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-        if (email==""){
-            prompt= "Por favor complete el campo";
-        }
-})
+
