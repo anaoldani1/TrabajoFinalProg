e6bb696499a9580e2f3284e569635e3dd65ede05
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2024-06-07 14:35:17 -0300

    register,css and some changes

--- js/register.js
+++ js/register.js
@@ -1,6 +1,26 @@
-let email = document.getElementById("email");
-let password = document.getElementById("password").value;
-let repeatPassword = document.getElementById("repeatPassword").value;
+let campoEmail= document.querySelector('#email')
+let campoPassword= document.querySelector('#password')
+let campoRepeat=document.querySelector ('#rePassword')
+let form= document.querySelector("#registerForm")
+let errorRe= document.querySelector (".invalid-feedback-rePassword")
+form.addEventListener('submit',function (evento){
+    evento.preventDefault()
 
+    if (campoEmail.value==""){
+        alert("Porfavor complete el campo de email")
+    }
+    else if (campoPassword.value==""){
+        alert("Porfavor complete el campo de contraseña")
+    }
+    else if (campoPassword.value.length<6){
+        alert("La contraseña tiene que tener al menos 6 caracteres")
+    }
+    else if (campoPassword!==campoRepeat){
+        errorRe.innerHTML= "<p>Las contraseñas no coinciden</p>"
+    }
+    else{
+        alert ("Felicidades has podidio completar el formulario")
+    }
+})
 
-
+sessionStorage.setItem('email', campoEmail.value)
