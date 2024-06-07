196e246892119d3cf62524a396a870cf736e488b
felixgp1 <fgonzalezprandi@udesa.edu.ar>
2024-06-07 11:24:23 -0300

    A

---
+++ js/category.js
@@ -0,0 +1,10 @@
+fetch(url)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
