1f93c44aeb8e84f6e61253e51bd93732d11a9b8b
anaoldani <anaoldani@Anas-MacBook-Air.local>
2024-06-13 19:50:21 -0300

    a

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 let se = document.querySelector(".productos") 
 let boton = document.querySelector(".desc")
+let comprar = document.querySelector(".agregara")
 
 fetch (`https://fakestoreapi.com/products`)
 .then(function(response){
@@ -12,7 +13,7 @@ fetch (`https://fakestoreapi.com/products`)
         se.innerHTML = "<h2>Su Carrito está Vacío</h2>"
         boton.style.display = "none";
     } else {
-       let datosParseados = JSON.parse(datosEnLocal) // como recupero los datos con el id de cada uno for? //
+        JSON.parse(datosEnLocal) // como recupero los datos con el id de cada uno for? //
         se.innerHTML = `
         <article>
             <img class="section-images" src="${data.image}">                 
@@ -20,7 +21,13 @@ fetch (`https://fakestoreapi.com/products`)
         <article class="text">
             <h3>${data.title}</h3>
             <p>$${data.price}</p>                
-        </article> `  
+        </article> `
+        comprar.addEventListener("click", function(e){
+            e.preventDefault()
+            localStorage.clear();
+            alert("GRACIAS POR SU COMPRA"); //no se ven el alert y eso//
+            window.location.href = 'index.html';
+        })
     }
     console.log(localStorage);
 
