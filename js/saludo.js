bab83454142a705b1eb1969ea70f1e6725000054
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2024-06-14 10:42:55 -0300

    6b finisheeed

--- js/saludo.js
+++ js/saludo.js
@@ -3,17 +3,15 @@ let userEmail = localStorage.getItem('email')
 
 document.querySelector("#welcomeMessage").innerText = "Bienvenido: " + userEmail;
 
-        // if (userEmail) {
-        //     welcomeMessageElement.innerHTML = ` <a href='#' class=logout-link> Logout </a> `;
-        //     document.querySelector('.logout-link').addEventListener('click', function (event) {
-        //         event.preventDefault();
-        //         localStorage.removeItem('email')
-        //     });
-        // } 
 
     let nav= document.querySelector('.barra')
     let disappear= document.querySelector ('.none')
     let disappear2= document.querySelector ('.none2')
+
+    console.log(nav);
+    console.log(disappear);
+    console.log(disappear2);
+
         if (userEmail==""){
             disappear.style.display= "block"
             disappear2.style.display= "block"
