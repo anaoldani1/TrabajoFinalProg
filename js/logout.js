186784da4602db94fd4cc1047477293ca3ca31d1
felixgp1 <fgonzalezprandi@udesa.edu.ar>
2024-06-17 21:05:03 -0300

    A

--- js/logout.js
+++ js/logout.js
@@ -2,22 +2,31 @@ let bienvenido = document.querySelector ('.logout-link')
 
 bienvenido.addEventListener('click' , function (){
      let nav= document.querySelector('.barra')
-     let disappear= document.querySelector ('.none')
-     let disappear2= document.querySelector ('.none2')
- 
+     let appear= document.querySelector ('.none')
+     let appear2= document.querySelector ('.none2')
+     let welcomemsj = document.querySelector ('.welcomeMessage')
+
+
+     
      document.querySelector(".logout-link").innerHTML = (" ")
 
+   
+     
+     localStorage.removeItem ('email"');
+
+
      console.log(nav);
-     console.log(disappear);
-     console.log(disappear2);
+     console.log(appear);
+     console.log(appear2);
  
          if (userEmail==""){
-             disappear.style.display= "none"
-             disappear2.style.display= "none"
+             appear.style.display= "none"
+             appear2.style.display= "none"
+             welcomemsj.style.display= "block"
          } else{
-             disappear.style.display= "block"
-             disappear2.style.display= "block"
-           
+             appear.style.display= "block"
+             appear2.style.display= "block"
+            
          } 
  
 }) ;
