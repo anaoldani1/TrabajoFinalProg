026d8d7c4e7d06228c79513b74113af4237e4467
felixgp1 <fgonzalezprandi@udesa.edu.ar>
2024-06-18 15:51:56 -0300

    borrar carpeta extra

--- js/category.js
+++ js/category.js
@@ -1,25 +1,12 @@
 let catelectro = document.querySelector ('<li> <a href="./category.html">ELECTRÓNICA</a></li>')
 
-catelectro.addEventListener ('click' , function(){
- fetch('https://fakestoreapi.com/products/category/electronics')
-            .then(function(response){
-                return response.json ();
-            } )
-            .then(function(data) {
-                console.log(data);
-                let hoodienegro = document.querySelector(".algo2")
-                
-            } )   
-    
-            .catch(function(err) {
-                console.log(err);
-            } )
-            
-
-})
+fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+            let hoodienegro = document.querySelector(".algo2") ; 
 
 
-
+ 
 
 
 
