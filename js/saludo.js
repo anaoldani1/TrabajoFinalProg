fee9d498fc1af6a4dd288c4000f110c22d711f07
anaoldani <anaoldani@Anas-MacBook-Air.local>
2024-06-20 17:34:19 -0300

    S

--- js/saludo.js
+++ js/saludo.js
@@ -11,7 +11,7 @@ document.querySelector("#welcomeMessage").innerText = "Bienvenido: " + userEmail
     console.log(nav);
     console.log(disappear);
     console.log(disappear2);
-
+    console.log(userEmail);
         if (userEmail==""){
             disappear.style.display= "block"
             disappear2.style.display= "block"
