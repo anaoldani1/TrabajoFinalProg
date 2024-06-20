7806286906dc4c193dbd4c36cf66987574622283
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2024-06-20 18:21:41 -0300

    modified legend

--- js/saludo.js
+++ js/saludo.js
@@ -1,21 +1,20 @@
 
 let userEmail = localStorage.getItem('email')
 
-document.querySelector("#welcomeMessage").innerText = "Bienvenido: " + userEmail;
-
-
     let nav= document.querySelector('.barra')
     let disappear= document.querySelector ('.none')
     let disappear2= document.querySelector ('.none2')
 
-    console.log(nav);
-    console.log(disappear);
-    console.log(disappear2);
-
         if (userEmail==""){
             disappear.style.display= "block"
             disappear2.style.display= "block"
-        } else{
+        } 
+        else if (userEmail==null){
+            disappear.style.display= "block"
+            disappear2.style.display= "block"
+        }
+        else{
+            document.querySelector("#welcomeMessage").innerText = "Bienvenido: " + userEmail;
             disappear.style.display= "none"
             disappear2.style.display= "none"
             nav.innerHTML+= '<li><a href= "#" class=logout-link> LOGOUT</li>'
